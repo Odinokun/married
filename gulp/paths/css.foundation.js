2ce9ceef3994710b1a6332c295ab938fb39d9bfd
odinokun <odinokun@gmail.com>
2021-02-01 20:14:25 +0200

    start

---
+++ gulp/paths/css.foundation.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = [
+  './node_modules/normalize.css/normalize.css',
+  './node_modules/slick-carousel/slick/slick.css',
+  './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
+];
---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+$(function() {
+
+  // begin SVG for IE
+  require('./modules/svg4everybody')();
+
+  // begin no-JS
+  require('./modules/noJs')();
+
+  // begin Scroll2Ancor
+  // require('./modules/scroll2ancor')();
+
+  // begin Slick slider
+  // require('./modules/slick')();
+
+  // begin Popup
+  // require('./modules/popup')();
+
+});
